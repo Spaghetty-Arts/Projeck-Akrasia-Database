c171c2ce2222956a54dcd81b26a3435bfb7bcc1f
fabian-nunes <fabiannunes12@gmail.com>
2021-06-01 19:00:46 +0100

    doing shit

--- sql script/script.sql
+++ sql script/script.sql
@@ -5,6 +5,15 @@ CREATE TABLE user (
 	email	 varchar(60) UNIQUE NOT NULL,
 	money	 int NOT NULL DEFAULT 100,
 	life	 int NOT NULL DEFAULT 100,
+	last_login date not null,
+	login_reward int not null default 0,
+	got_reward tinyint not null default 0,
+	active tinyint not null default 0,
+	token varchar(60) unique,
+	winP int not null  default 0,
+	loseP int not null  default 0,
+	rankP int not null  default 1,
+	last_action timestamp not null,
 	PRIMARY KEY(id)
 );
 
@@ -16,6 +25,16 @@ CREATE TABLE reset (
 	PRIMARY KEY(id)
 );
 
+
+create table history
+(
+	id INTEGER auto_increment primary key,
+	`match` INTEGER not null,
+	winner int not null,
+	loser int not null,
+	moment timestamp not null
+);
+
 ALTER TABLE reset ADD CONSTRAINT reset_fk1 FOREIGN KEY (user_id) REFERENCES user(id);
 
 DROP EVENT IF EXISTS `delete_old_request`;
@@ -24,4 +43,21 @@ STARTS '2010-01-01 00:00:00'
 DO
 DELETE FROM `reset` where DATEDIFF(now(),`request_date`) > 1;
 
+
 ALTER EVENT `delete_old_request` ON  COMPLETION PRESERVE ENABLE;
+
+DROP EVENT IF EXISTS `delete_old_login`;
+CREATE EVENT `delete_old_login`  ON SCHEDULE EVERY 12 hour
+STARTS '2010-01-01 00:00:00'
+DO
+UPDATE user set token = null and active = 0 where active = 1;
+
+ALTER EVENT `delete_old_login` ON  COMPLETION PRESERVE ENABLE;
+
+DROP EVENT IF EXISTS `delete_old_user`;
+CREATE EVENT `delete_old_user`  ON SCHEDULE EVERY 1 hour
+STARTS '2010-01-01 00:00:00'
+DO
+UPDATE user set token = null and active = 0 where TIMESTAMPDIFF(HOUR,last_action,NOW()) > 1;
+
+ALTER EVENT `delete_old_user` ON  COMPLETION PRESERVE ENABLE;
